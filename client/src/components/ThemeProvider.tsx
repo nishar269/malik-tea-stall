e080300f94bb7e64fd7fc2886108eaca73a678b1
nishar shaik <165269258+nishar269@users.noreply.github.com>
2026-02-08 15:36:27 +0530

    Force ThemeProvider Rebuild - Check This Build

--- client/src/components/ThemeProvider.tsx
+++ client/src/components/ThemeProvider.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
+// Wrapper for next-themes provider to avoid type issues
 export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
